59cc93cffef32ec8d6cc0ebe48c6e0b8f7dcb33e
Prathmesh Bodas <prathmb@amazon.com>
2024-05-05 11:25:20 -0700

    Added code change for actual split

--- MatrixMultiplication/src/MyProgram.kt
+++ MatrixMultiplication/src/MyProgram.kt
@@ -1,7 +1,12 @@
 val inputArray = intArrayOf(40, 20, 30, 10, 30)
-// 40 * 20, 20 * 30, 30 * 10, 10 * 30
+// 40 * 20, (20 * 30, 30 * 10), 10 * 30  =>  6000
+
+// 40 * 20, 20 * 10, 10 * 30  => 8000
+
+// 40 * 10, 10 * 30     => 12000
 
 val mem = Array(inputArray.size) { IntArray(inputArray.size) {-1} }
+val split = Array(inputArray.size) { IntArray(inputArray.size) {-1} }
 
 fun main(args:Array<String>) {
 
@@ -10,6 +15,8 @@ fun main(args:Array<String>) {
     println(multiplicationCount(inputArray, 0, inputArray.size - 1))
 
     mem.map { it.map { print("$it ") }; println() }
+    println("****".repeat(10))
+    split.map { it.map { print("$it ") }; println() }
 }
 fun multiplicationCount(inputArray: IntArray, start: Int, end: Int) : Int {
     if  (end - start <= 1) {
@@ -25,14 +32,17 @@ fun multiplicationCount(inputArray: IntArray, start: Int, end: Int) : Int {
     }
 
     var minimumCount = Integer.MAX_VALUE
+    var splitIndex = -1
 
     for (i in start + 1..< end) {
         val left = multiplicationCount(inputArray, start, i)
         val right = multiplicationCount(inputArray, i, end)
         minimumCount = Math.min(minimumCount, left + right + inputArray[start] * inputArray[i] * inputArray[end])
+        splitIndex = i
     }
 
     mem[start][end] = minimumCount
+    split[start][end] = splitIndex
 
     return minimumCount
 }
